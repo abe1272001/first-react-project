7e6f5291be9929cde9e18abb170e4a6d24cf76a9
Abe <abe1272001@gmail.com>
2020-10-25 03:06:50 +0800

    my first react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Tweet from './Tweet';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  useState()
+
+  const [users, setUsers] = useState([
+    {name: "Abe Chen", message: "It's my first React Project"},
+    {name: "Jazmine", message: "Wow, It's nice"},
+    {name: "Donarld Jump", message: "MAKE ME GREAT AGAIN!!"}
+  ])
+
+
+  return(
+    //類似React.createElement('div')
+    <div className="app"> 
+      {users.map(user => (
+        <Tweet name={user.name} message={user.message}/>
+      ))}
     </div>
   );
-}
+};
 
-export default App;
+export default App
